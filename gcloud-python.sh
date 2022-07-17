3b5bd51ac15c280c52c409f1bdb157ec67eb71f7
Shailesh <50418172+sendtoshailesh@users.noreply.github.com>
2022-07-17 21:10:18 +0530

    Create gcloud-python.sh

---
+++ gcloud-python.sh
@@ -0,0 +1,10 @@
+pip install virtualenv
+virtualenv shailesh-1-python
+source shailesh-1-python/bin/activate
+shailesh-1-python/bin/pip install google-cloud-bigtable
+  
+  
+cd cloudshell_open/python-bigtable/
+cd /home/admin_/cloudshell_open/python-bigtable/samples/quickstart
+
+python main.py shailesh-1 bt-test
